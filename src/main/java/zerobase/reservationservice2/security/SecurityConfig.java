f4f0a64720f682d1942c7d0eb961aaceb40e565e
pjy3012 <ppp0868@naver.com>
2022-08-18 19:52:23 +0900

    주석 제거

--- src/main/java/zerobase/reservationservice2/security/SecurityConfig.java
+++ src/main/java/zerobase/reservationservice2/security/SecurityConfig.java
@@ -1,2 +1,45 @@
-package zerobase.reservationservice2.security;public class SecurityConfig {
+package zerobase.reservationservice2.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter authenticationFilter;
+
+    //실제 서비스 관련 url
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**/signup", "/**/signin").permitAll()
+                .antMatchers("/**/forgot/**, /**/reset/**").permitAll()
+                .and()
+                .addFilterBefore(authenticationFilter ,UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+
+    // 개발 관련 url
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web -> web.ignoring().antMatchers("/h2-console/**"));
+    }
+
 }
--- src/main/java/zerobase/reservationservice2/security/TokenProvider.java
+++ src/main/java/zerobase/reservationservice2/security/TokenProvider.java
@@ -1,2 +1,93 @@
-package zerobase.reservationservice2.security;public class Tokenprovider {
+package zerobase.reservationservice2.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import zerobase.reservationservice2.service.MemberService;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class TokenProvider {
+
+    @Value("${spring.jwt.secret}")
+    private String secretKey;
+
+    private static final String KEY_ROLES = "roles";
+    private static final long TOKEN_EXPIRE_TIME = 1000 * 60 * 60; // 1 hour
+    private final MemberService memberService;
+
+
+    /*
+     * 토근 생성(발급)
+     */
+    public String generateToken(String userId, String roles) {
+
+        Claims claims = Jwts.claims().setSubject(userId);
+        claims.put(KEY_ROLES, roles);
+
+
+        var key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+        //토큰 생성시간
+        var now = new Date();
+        //토큰 만료시간
+        var expiredDate = new Date(now.getTime() + TOKEN_EXPIRE_TIME);
+
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiredDate)
+                .signWith(key)
+                .compact();
+    }
+
+    //jwt 토큰으로 부터 인증정보를 받아오는 메소드
+    public Authentication getAuthentication(String jwt) {
+        UserDetails userDetails = memberService.loadUserByUsername(getUserId(jwt));
+
+        // spring 에서 지원하는 token
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    // token 에 저장 돼 있는 useId 값을 가져오는 메소드
+    public String getUserId(String token) {
+        return this.parseClaims(token).getSubject();
+    }
+
+    // 토큰의 유효성 체크 ( 토큰이 비어 있는지, 토큰의 만료 시간이 현재 시간보다 전인지)
+    public boolean validateToken(String token) {
+        if (!StringUtils.hasText(token)) {
+            return false;
+        }
+
+        var claims = this.parseClaims(token);
+        return !claims.getExpiration().before(new Date());
+    }
+
+    // token 확인
+    private Claims parseClaims(String token) {
+
+        var key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+        try {
+            return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+        } catch (ExpiredJwtException e) {
+            // TODO
+            return e.getClaims();
+        }
+    }
 }
